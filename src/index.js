d35ce93c033a6f710d8c677370f5dcd2e059606e
alchematrix0 <alchematrix0@gmail.com>
2019-07-24 15:24:46 -0700

    dont bother importing no index.css file we can use App.css as index.js will not be concerned with styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 // service worker is commented out as we are creating one just for push purposes
