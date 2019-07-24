09efad4f295e7a8bc7abb6f9d60cd5645f4886c0
alchematrix0 <alchematrix0@gmail.com>
2019-07-24 15:22:45 -0700

    include a config file that we can use to quick style this app for other properties

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const config = {
+  "name": "nemesis",
+  "menuPath": "menu.js",
+  "colors": {
+    "main": "#89c9e5",
+    "secondary": "",
+    "text": "#363636",
+    "background": "#febf62"
+  },
+  "payments": ["square"]
+}
